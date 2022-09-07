408d6c6411b82d6384aab79f0342ac83419b2191
Gabriel Ade <themarveloustobi@gmail.com>
2022-09-07 21:45:30 +0100

    Added read me
    Created class for the code and added readme file

--- footy_db.py
+++ footy_db.py
@@ -1,152 +1,166 @@
-
+"""
+Importing necessary libraries
+"""
 import pandas as pd 
 
 import sqlite3  
 
 """
-Outlining the table structure
+Creating a database class with different methods,
+each of which perform a specific  function
 """
-table_data = ("""CREATE TABLE ballers (
-    first_name text,
-    last_name text, 
-    position text,
-    club text,
-    country text
+	
+class DB():
+	"""
+	creating constructor object for the DB class
+	"""
+	def __init__(self):
+		self.connect = None
+
+	"""
+	This function creates a connection to a new or pre-existing database
+	params: database name 
+	returns: connection to database
+	rtype: object
+	"""
+	def create_connection(self, db_name): 
+		
+		try:
+			self.connect = sqlite3.connect(db_name)
+			print("Connection successfully created")
+		except sqlite3.Error:
+			print("Unable to create connection")
+			self.connect = sqlite3.connect(db_name)
+		return self.connect
+
+	"""
+	This function creates a database table
+	params: table schema
+	returns: creats database table
+	rtype: object
+	"""
+	def create_table(self, table_data):
+		cursor = self.connect.cursor()
+		try:
+			print("creating table...")
+			cursor.execute(table_data)
+			self.connect.commit()
+			print("table created")
+		except:
+			print('Table already exists')
+		self.connect.commit()
+
+	
+	"""
+	This function adds multiple items to a table in the database
+	params: list of items to be added
+	returns: populated database
+	rtype: object
+	"""
+	def add_items(self, items_list):
+		cursor = self.connect.cursor()
+		cursor.executemany("INSERT INTO ballers VALUES (?,?,?,?,?)", items_list)
+		self.connect.commit()
+	
+	"""
+	This function displays table and its data
+	params: None
+	return: database table 
+	rtype: object
+	"""
+	def show_table(self):
+		cursor = self.connect.cursor()
+		data = cursor.execute("SELECT * FROM ballers").fetchall()
+		return data
+
+	"""
+	This function deletes a row from a table using the row id 
+	params: database connection, id of row to be deleted
+	returns: None
+	"""
+	def delete_row_by_id(self, id):
+		cursor = self.connect.cursor()
+		cursor.execute("DELETE FROM  WHERE rowid = (?)", id)
+		self.connect.commit()
+	
+	"""
+	This function deletes the entire table from the database
+	params: 
+	returns: None
+	"""
+	def delete_table(self):
+		cursor = self.connect.cursor()
+		try:
+			print('Deleting Table...')
+			cursor.execute("DROP TABLE ballers")
+			print("Table Deleted")
+		except sqlite3.Error:
+			print("Unable to delete table")
+
+		self.connect.commit()
+
+
+	def create_csv_data(self, data, file_name):
+		writer = []
+		for item in data:
+			dict_writer = {
+				"first_name" : item[0],
+				'last_name' : item[1],
+				'position' : item[2],
+				"club" : item[3],
+				"country" : item[4]
+
+			}
+			writer.append(dict_writer)
+		Baller_df = pd.DataFrame(writer, columns=["first_name", "last_name", "position", "club" , "country" ])
+		print("Converting to csv in progress")
+		Baller_df.to_csv(file_name, index=False)
+		print("Conversion to csv is successful")
+
+
+
+
+"""
+Outlining the table schema
+"""
+table_data = ("""CREATE TABLE IF NOT EXISTS ballers  (
+first_name text,
+last_name text, 
+position text,
+club text,
+country text
 )""")
 
 """
 List of items to be inserted into the table
 """
 ballers_list = [
-	('Neymar', 'Jr', 'FW', 'PSG', 'Brazil'),
-	('Kevin', 'De Bruyne', 'AM', 'Man City', 'Belgium'),
-	('Joao', 'Felix', 'SS', 'Aletico', 'Portugal'),
-	('Toni', 'Kroos', 'CM', 'Madrid', 'Germany'),
-	('Gabriel', 'Jesus', 'FW', 'Arsenal', 'Brazil'),
-	('Bernado', 'Silva', 'AM', 'Man City', 'Portugal'),
-	('Phil', 'Jones', 'CB', 'Man Utd', 'England'),
-	('Ferran', 'Torres', 'FW', 'Barcelona', 'Spanish'),
-	('Gianluigi', 'Buffon', 'GK', 'Parma', 'Italy'),
-	('Lionel', 'Messi', 'FW', 'PSG', 'Argentina')
+('Neymar', 'Jr', 'FW', 'PSG', 'Brazil'),
+('Kevin', 'De Bruyne', 'AM', 'Man City', 'Belgium'),
+('Joao', 'Felix', 'SS', 'Aletico', 'Portugal'),
+('Toni', 'Kroos', 'CM', 'Madrid', 'Germany'),
+('Gabriel', 'Jesus', 'FW', 'Arsenal', 'Brazil'),
+('Bernado', 'Silva', 'AM', 'Man City', 'Portugal'),
+('Phil', 'Jones', 'CB', 'Man Utd', 'England'),
+('Ferran', 'Torres', 'FW', 'Barcelona', 'Spanish'),
+('Gianluigi', 'Buffon', 'GK', 'Parma', 'Italy'),
+('Lionel', 'Messi', 'FW', 'PSG', 'Argentina')
 ]
 
-"""
-This function creates a connection to a new or pre-existing database
-params: database_name 
-returns: connection to database
-rtype: object
-"""
-def create_connection(db_name): 
-	conn = None
-	try:
-		conn = sqlite3.connect(db_name)
-		print("COnnection successfully created")
-	except sqlite3.Error:
-		print("Unable to create connection")
-	return conn
-"""
-This function creates a database table
-params: table_schema, connection
-"""
-def create_table(connection, table_data):
-	c = connection.cursor()
-	print("creating table...")
-	c.execute(table_data)
-	connection.commit()
-	print("table created")
-
 
 """
-This function displays table data
-params: database connection
-return: table data
+Creating a main function to call all the class methods 
 """
-def show_table(connection):
-	c = connection.cursor()
-	data = c.execute("SELECT * FROM ballers").fetchall()
-	return data
-
-
-"""
-This function creates a csv file using specified table data
-params: table data, name of file to be created
-return: csv file
-rtype: csv
-"""
-def create_csv_data(data, file_name):
-	writer = []
-	for item in data:
-		dict_writer = {
-			"first_name" : item[0],
-			'last_name' : item[1],
-			'position' : item[2],
-			"club" : item[3],
-			"country" : item[4]
-
-		}
-		writer.append(dict_writer)
-	Baller_df = pd.DataFrame(writer, columns=["first_name", "last_name", "position", "club" , "country" ])
-	print("Converting to csv in progress")
-	Baller_df.to_csv(file_name, index=False)
-	print("Conversion to csv is successful")
+def main():
+	db = DB() #creates an instance of the class 
+	db.create_connection('Footy_db')
+	db.create_table(table_data)
+	db.add_items(ballers_list)
+	print(db.show_table())
+	db.create_csv_data(ballers_list, 'players_data')
 
 """
-This function deletes a row from a table using the specified id number
-params: database connection, id of row to be deleted
-"""
-def delete_row_by_id(connection, id):
-	c = connection.cursor()
-	c.execute("DELETE FROM TABLE WHERE rowid = (?)", id)
-	connection.commit()
-
-"""
-This function adds multiple items to
-params: database connection, id of row to be deleted
-"""
-def add_items(connection, items_list):
-	c = connection.cursor()
-	c.executemany("INSERT INTO ballers VALUES (?,?,?,?,?)", items_list)
-	connection.commit()
-
-"""
-This function deletes the entire table from the database
-params: database connection
-"""
-def delete_table(connection):
-	c = connection.cursor()
-	print("Deleting table...")
-	c.execute("DROP TABLE ballers")
-	print("Table deleted...")
-	connection.commit()
-
-"""
-Testing the functions 
+Invoking the main function
 """
 if __name__ == "__main__":
-	connection = create_connection("Footy_db")
-
-	create_table(connection, table_data)
-
-	add_items(connection, ballers_list)
-	
-
-	data = show_table(connection)
-
-	for items in data:
-		print(items)
-	
-	create_csv_data(ballers_list, "Ballers_data")
-
-	
-	
-
-
-
-
-
-
-
-
-
-
-
+	main()
